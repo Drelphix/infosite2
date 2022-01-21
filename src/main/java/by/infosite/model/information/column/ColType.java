4cd3f02819263a462c1182004ddf542fda76ed1e
Alexey <alexeydemeshko@gmail.com>
2022-01-21 16:58:04 +0300

    add JpaRepositories

--- src/main/java/by/infosite/model/information/column/ColType.java
+++ src/main/java/by/infosite/model/information/column/ColType.java
@@ -1,5 +1,10 @@
 package by.infosite.model.information.column;
 
 public enum ColType {
-    TEXT, HIDDEN_TEXT, LINK, PICTURE, FILE,
+    TEXT, //standard text format of cells
+    HIDDEN_TEXT, //text format of cells hidden when displayed
+    LINK, //Displayed as a link, shortened display
+    PICTURE, //Image display
+    FILE, //Downloading a file when clicked
+    UNIQUE //All cells in the column are unique
 }
